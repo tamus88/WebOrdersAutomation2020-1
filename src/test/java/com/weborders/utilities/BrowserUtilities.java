6e68047fcb4509e1794bdad4c5ad7943182c25dc
tamus88 <tamus88@mail.ru>
2020-04-08 20:41:55 -0400

    added utilities switching windows

--- src/test/java/com/weborders/utilities/BrowserUtilities.java
+++ src/test/java/com/weborders/utilities/BrowserUtilities.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 public class BrowserUtilities {
     /**
@@ -109,5 +110,15 @@ public class BrowserUtilities {
             e.printStackTrace();
         }
         return path;
+    }
+
+    public static void switchWindow(String title){
+        Set<String> windowHandles = Driver.getDriver().getWindowHandles();
+        for (String window :windowHandles) {
+            Driver.getDriver().switchTo().window(window);
+            if(Driver.getDriver().getTitle().equals(title)){
+                break;
+            }
+        }
     }
 }
